0f7f9ae4ad52fff5ba186651bcf74e0a0e9cb664
Mubeshira <159130265+Mubeshira@users.noreply.github.com>
2024-03-08 20:55:39 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,33 @@
+// Case Sensitive
+
+// camelCase Example
+let personName: string = "Mubeshira";
+let rollNumber: number = 475625;
+console.log(personName , rollNumber);
+
+// snake_case Example
+let father_name: string = "Najeeb Malik";
+console.log(father_name);
+
+
+// PascalCase Example
+let FavoriteColor: string = "Red";
+let CarColorRed: boolean = false;
+console.log(FavoriteColor , CarColorRed);
+
+
+// Arrays Example
+let numbersList: number[] = [1,2,3,4,5,6,7,8,9,10];
+console.log(numbersList[0] , numbersList[4]);
+
+
+// Array change
+let animalsList: string[] = ["Lion" , "Monkey" , "Zebra" , "Elephant" , "Tiger" , "Crocodile" , "Panda" ];
+animalsList[3] = "Giraffe";
+console.log(animalsList);
+
+
+
+
+
+
